0e76539c1fdca0627039834d0dfe5e635850aca0
satriaputrafebriansyah <satriaputrafebriansyah@gmail.com>
2020-03-29 08:00:59 +0700

    first commit

---
+++ app/Person.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    Protected $table = 'persons';
+    Protected $fillable = ['first_name','last_name'];
+}
